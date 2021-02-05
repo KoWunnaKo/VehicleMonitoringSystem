399240307bb32a2f1cd3fa65b622ea4d48afc468
SergeyRoyt <sergey.royt@mail.ru>
2021-02-05 23:59:22 +0300

    [Backend] Remove razor pages

--- VehicleMonitoringSystemWeb/VehicleMonitoringSystemServer/VehicleMonitoringSystemServer/Startup.cs
+++ VehicleMonitoringSystemWeb/VehicleMonitoringSystemServer/VehicleMonitoringSystemServer/Startup.cs
@@ -1,10 +1,6 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using VehicleMonitoringSystemServer.Data;
 using Microsoft.Extensions.Configuration;
@@ -39,9 +35,6 @@ namespace VehicleMonitoringSystemServer
                 .AddIdentityServerJwt();
 
             services.AddControllers();
-
-            // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,30 +54,13 @@ namespace VehicleMonitoringSystemServer
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseSpaStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthentication();
             app.UseIdentityServer();
             app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-            });
 
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
-                }
-            });
         }
     }
 }
