5fa50e43c82dd193717effdd75f748a7e1d9c76e
SergeyRoyt <sergey.royt@mail.ru>
2021-03-05 13:09:47 +0300

    Simple Google Map

--- VehicleMonitoringSystemWeb/vms_frontend/src/pages/Home/index.tsx
+++ VehicleMonitoringSystemWeb/vms_frontend/src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import { withAuthorization } from "../../firebase/withAuthorization";
 import {StylesDictionary} from "../../utils/StylesDictionary";
 import {AppController} from "../../controllers/AppController";
 import Role from "../../models/Role";
+import SimpleMap from "../../components/Map/SimpleMap";
 
 class HomeComponent extends React.Component {
   constructor(props: any) {
@@ -12,8 +13,9 @@ class HomeComponent extends React.Component {
   public render() {
       return (
       <div style={styles.container}>
-        <h2>Home Page</h2>
-        <p>The Home Page is accessible by every signed in user.</p>
+        {/*<h2>Home Page</h2> */}
+        {/*<p>The Home Page is accessible by every signed in user.</p>*/}
+        <SimpleMap/>
       </div>
     );
   }
@@ -22,7 +24,7 @@ class HomeComponent extends React.Component {
 const styles: StylesDictionary  = {
     container: {
         flex: 1,
-        backgroundColor:'#b642f4'
+        // backgroundColor:'#b642f4'
     }
 };
 
