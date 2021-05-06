2b8280eb50fef76ac141fadbc1cc53c74a99e612
SergeyRoyt <sergey.royt@mail.ru>
2021-05-06 11:47:58 +0300

    [Web-front] Role division #54 CompanySettings withAuthorization

--- VehicleMonitoringSystemWeb/vms_frontend/src/pages/companySettings/index.tsx
+++ VehicleMonitoringSystemWeb/vms_frontend/src/pages/companySettings/index.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import {CompanySettingsForm} from "./companySettingsForm";
+import {withAuthorization} from "../../firebase/withAuthorization";
 
-export const CompanySettings = () => (
-    <div>
-        <h1>Company Settings</h1>
-        <CompanySettingsForm/>
-    </div>
-);
+export const CompanySettingsComponent : React.FunctionComponent = () => {
+    return (
+        <div>
+            <h1>Company Settings</h1>
+            <CompanySettingsForm/>
+        </div>
+    );
+}
+
+const authCondition = (authUser: any) => !!authUser;
+export const CompanySettings = withAuthorization(authCondition)(CompanySettingsComponent);
